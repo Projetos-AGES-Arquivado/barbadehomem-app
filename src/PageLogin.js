3949847ea14c4e475ab0ebcef8e3a475ba8895be
Joseph Affeldt <joseph.affeldt@dbccompany.com.br>
2020-03-27 22:09:24 -0300

    Iniciando branch

---
+++ src/PageLogin.js
@@ -0,0 +1,17 @@
+
+
+export default class PageLogin extends React.Component {
+    constructor() {
+      super();
+    }
+  
+    render() {
+      return (
+        <div className='App'>
+          <header className='App-header'>
+            Tele inicial
+          </header>
+        </div>
+      );
+    }
+  }
