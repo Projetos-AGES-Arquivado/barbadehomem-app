f15c1743570717bcb04443c4fd8336a2bbe8c0ed
Marlon Saldanha <marlon.saldanha@acad.pucrs.br>
2020-04-03 22:52:34 -0300

    corrigido arquivos faltantes

--- src/plugins/firebase.js
+++
@@ -1,6 +0,0 @@
-import * as firebase from 'firebase';
-import 'firebase/firestore';
-
-firebase.initializeApp(JSON.parse(process.env.REACT_APP_FIREBASE_CREDENTIALS));
-
-export const firestore = firebase.firestore();
