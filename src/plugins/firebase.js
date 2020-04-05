b9b095902afa387a1d2d4176d28e8db6ecc6e361
Giuseppe Menti <mentifg@gmail.com>
2020-04-05 18:41:59 -0300

    Read firestore config from env

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,16 +1,6 @@
 import * as firebase from 'firebase';
 import 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyDKphJSwCM42BL2noQ6aSR4UXszKUAqnMg',
-  authDomain: 'barba-de-homem.firebaseapp.com',
-  databaseURL: 'https://barba-de-homem.firebaseio.com',
-  projectId: 'barba-de-homem',
-  storageBucket: 'barba-de-homem.appspot.com',
-  messagingSenderId: '447109259878',
-  appId: '1:447109259878:web:31b9a82d94b0beccc6c3a7',
-};
-
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(JSON.parse(process.env.REACT_APP_FIREBASE_CREDENTIALS));
 
 export const firestore = firebase;
