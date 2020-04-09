7d910b2597b36e65a99d78c23552eac48352c1e7
Deyvisson Rodrigues <deyvisson.rodrigues@acad.pucrs.br>
2020-04-09 20:52:35 -0300

    Adicionado foto de perfil padrao do usuario; Ajustado responsividade para intervalos de tamanhos(celular,tablet e desktop) - pendente ajustes finos na responsividade

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import '../../css/home-page.css';
 import '../../css/button.css'
+import '../../css/grid.css';
 import Image from '../../components/Image'
 import Logo from '../../img/logo.png';
+import ProfilePhoto from '../../img/default_photo.png';
 
 export default class Home extends React.Component {
   constructor(){
@@ -37,12 +39,19 @@ export default class Home extends React.Component {
     return (
 
         <div className="background" alt="">
-          <div className="float">
-            <div className="logo">
-              <Image src={Logo} alt="Logo"></Image>
-            </div>
+
+          <div className="logo">
+            <Image src={Logo} alt="Logo"></Image>
           </div>
-          <div className="title"> <h1>Bem-Vindo, Usuário</h1></div>
+
+          <div className="profile-photo">
+            <Image src={ProfilePhoto} alt="Profile photo"></Image>
+          </div>
+
+          <div className="title"> <h1> Bem-Vindo, Usuário </h1> 
+          
+          </div>
+
           <div className="div-buttons">
             <button className="button"
               onClick={this.handleCutRequest}>Solicitar Corte
