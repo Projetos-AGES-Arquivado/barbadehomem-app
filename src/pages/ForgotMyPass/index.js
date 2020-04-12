c855c9ec43fb1bc3c5d15b82e2c5a43dc8c89f03
Marlon Saldanha <marlon.saldanha@acad.pucrs.br>
2020-04-12 17:50:29 -0300

    adicionado o sistema de recuperacao de senha

--- src/pages/ForgotMyPass/index.js
+++ src/pages/ForgotMyPass/index.js
@@ -1,16 +1,39 @@
-import React from "react";
-import { useHistory } from "react-router-dom";
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { resetPassword } from '../../store/auth/actions';
 
 export default function ForgotMyPass() {
   const history = useHistory();
+  const [email, setEmail] = useState('');
+
+  function handleResetPassword() {
+    try {
+      resetPassword(email);
+
+      alert(
+        'Email enviado. Caso esteja correto, você receberá um link com uma redefinição de senha.'
+      );
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
 
   function handleGoBack() {
-    history.goBack();
+    history.push('/');
   }
 
   return (
     <div>
-      <h1>Estou no Esqueci minha senha</h1>
+      <h2>Informe seu endereço de email:</h2>
+      <br />
+      <input
+        value={email}
+        type="email"
+        onChange={e => setEmail(e.currentTarget.value)}
+      />{' '}
+      <button onClick={handleResetPassword}>Enviar</button>
+      <br />
       <button onClick={handleGoBack}>Voltar</button>
     </div>
   );
