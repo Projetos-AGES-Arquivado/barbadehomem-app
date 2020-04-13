b2d746afc7bf19ad7cdf209b48878f6150fcb4c4
Marlon Saldanha <marlon.saldanha@acad.pucrs.br>
2020-04-12 22:48:38 -0300

    corrigido estrutura de pastas

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './plugins/serviceWorker';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './store/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
