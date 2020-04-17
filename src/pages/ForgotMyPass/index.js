dc342129be25ecd3a654045df8abac02079dfa91
Joseph Affeldt <joseph.affeldt@dbccompany.com.br>
2020-04-17 04:25:28 -0300

    realizada style da página, com o que o pessoal já tinha realizado.

--- src/pages/ForgotMyPass/index.js
+++ src/pages/ForgotMyPass/index.js
@@ -2,7 +2,14 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
+import '../../css/forgot-page.css';
+import '../../css/header.css';
+
+import Background from '../../components/Background';
+
 import { resetPassword } from '../../store/auth/actions';
+import Button from '../../components/Button';
+import Header from '../../components/Header';
 
 export default function ForgotMyPass() {
   const history = useHistory();
@@ -12,7 +19,7 @@ export default function ForgotMyPass() {
 
   function handleResetPassword() {
     try {
-      dispatch(resetPassword( email ));
+      dispatch(resetPassword(email));
 
       alert(
         'Email enviado. Caso esteja correto, você receberá um link com uma redefinição de senha.'
@@ -29,17 +36,22 @@ export default function ForgotMyPass() {
   }
 
   return (
-    <div>
-      <h2>Informe seu endereço de email:</h2>
-      <br />
-      <input
-        value={email}
-        type="email"
-        onChange={e => setEmail(e.currentTarget.value)}
-      />{' '}
-      <button onClick={handleResetPassword}>Enviar</button>
-      <br />
-      <button onClick={handleGoHome}>Voltar</button>
-    </div>
+    <Background>
+      <Header text="Recuperar senha"/>
+
+      <div className="div-forgotmypass">
+        <div className="div-input-forgot">
+          <h3>Informe seu endereço de email</h3>
+          <input
+            value={email}
+            type="email"
+            onChange={e => setEmail(e.currentTarget.value)}
+          />
+        </div>
+        <Button classe="button" text="Enviar" event={handleResetPassword} />
+        <Button classe="button" text="Voltar" event={handleGoHome} />
+      </div>
+
+    </Background>
   );
 }
