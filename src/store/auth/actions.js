10654b5f029b06584e58d4830395eae03b26fcf9
Delmar Lucas Dorneles Quaresma <luk.q321@gmail.com>
2020-04-27 00:49:08 -0300

    Implementacao do login com o google

--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -151,21 +151,20 @@ export function signinWithGoogle(){
   const provider = new firestore.auth.GoogleAuthProvider();
 
   return async dispatch =>{
-  firestore.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
-}
+    const res = await firestore.auth().signInWithPopup(provider);
+
+    const { displayName: name, email, uid } = res.user;
+
+    const user = await firestore.firestore().collection('users').doc(uid).get();
+    if(user.exists){
+      await dispatch(fetchUser(uid));
+    }else{
+      const publicData = {
+        email,
+        name,
+      };
+      await firestore.firestore().collection('users').doc(uid).set(publicData);
+    }
+
+  };
 }
