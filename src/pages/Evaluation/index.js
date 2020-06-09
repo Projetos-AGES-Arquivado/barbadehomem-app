9477f6f2baa06910154a5ae27e6792ea2d6bf5f5
joao <joaogranzinoli@gmail.com>
2020-06-09 07:25:43 -0300

    Concluido animacao de estrela de avaliacao

--- src/pages/Evaluation/index.js
+++ src/pages/Evaluation/index.js
@@ -25,6 +25,29 @@ export default function Evaluation() {
             <FiCornerDownLeft size={25} onClick={handleGoBack} />
             <h1>Avalie sua experiencia</h1>
           </header>
+            
+            <div class = "rating">
+                <input type = "radio" name ="star" id ="star1" /><label for = "star1">
+                </label>
+                <input type = "radio" name ="star" id ="star2" /><label for = "star2">
+                </label>
+                <input type = "radio" name ="star" id ="star3" /><label for = "star3">
+                </label>
+                <input type = "radio" name ="star" id ="star4" /><label for = "star4">
+                </label>
+                <input type = "radio" name ="star" id ="star5" /><label for = "star5">
+                </label>
+            </div>
+
+          <div className="div_input">
+            <span>Comente ou sugira algo que possamos melhorar</span>
+            <Input type = 'text'/>
+          </div>
+
+          <div className="divbutton">
+             <Button>Submeter Avaliação</Button>
+          </div>
+
         </div>  
     );
 }
