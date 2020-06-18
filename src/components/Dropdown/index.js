8ccc03a36be466202b288b90d60e96a1d49976bc
Arthur Lamberti <arthurlamberti001@gmail.com>
2020-06-18 01:19:41 -0300

    colocado id no dropdown

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -9,7 +9,7 @@ const DropDown = (props) => {
             <span className="span-dropdown">{props.selected === '' ? (<>Escolha um meio de pagamento <FiChevronDown/></>): props.selected}</span>
             <div className="dropdown-content">
                 {
-                    props.options.map((opcao) => <button className="item" onClick={()=>props.onClick(opcao.method)}>{opcao.method}</button>)
+                    props.options.map((opcao) => <button key={opcao.id} className="item" onClick={()=>props.onClick(opcao.method)}>{opcao.method}</button>)
                 }
             </div>
         </div>
